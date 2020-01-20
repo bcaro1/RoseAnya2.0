b56682001c64cdf96b5098d980af629bb2335004
Joseph Winemberg <42650208+WinembergJo@users.noreply.github.com>
2020-01-20 12:37:25 -0500

    Quest and Quest System Framework made

---
+++ Assets/Tech Team/Scripts/JosephScripts/Quest_Joseph.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Quest_Joseph
+{
+    #region Public
+    public string QuestName;
+    public string ElementUsed;
+    public bool CanBeTurnedIn = false;
+    public bool Success = false;
+    //Possibly Consider Adding Rewards to Quests
+    #endregion
+
+    #region Private
+    private Transform StartLocation;
+    //Possibly a Reference to Fungus?
+    #endregion
+
+    public abstract void InitializeTask();
+
+    public abstract void IncrementTask();
+
+    public abstract void CompleteTask();
+
+    public abstract void RestartOnFail();
+}
