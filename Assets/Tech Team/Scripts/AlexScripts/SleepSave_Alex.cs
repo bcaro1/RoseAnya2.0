eff1f3d619228c17679d8b0471eaefd88afeface
Alex <heitzalex@yahoo.com>
2020-02-16 01:06:00 -0500

    StoryManager + Sleep/Save checker (WIP)
    Creating a manager to keep track of the story timeline.
    
    Creating a checker that is responsible for the player "sleeping" and starting the next quest.

---
+++ Assets/Tech Team/Scripts/AlexScripts/SleepSave_Alex.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepSave_Alex : MonoBehaviour
+{
+
+    [Tooltip("Script will find Player")]
+    public GameObject Player;
+    public GameObject textUI;
+    public bool inRadius;
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player"); // Grabs Player
+    }
+
+    void Update()
+    {
+        if (inRadius && Input.GetKeyDown(KeyCode.S)) // hardcoded, will change -@AH
+        {
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // if the player is in radius
+        {
+            textUI.SetActive(true);
+            inRadius = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // if the player is out of radius
+        {
+            textUI.SetActive(false);
+            inRadius = false;
+        }
+    }
+}
---
+++ Assets/Tech Team/Scripts/AlexScripts/StoryManagement_Alex.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManagement_Alex : MonoBehaviour
+{
+    #region Public
+    public bool tutorial, firstQuest, secondQuest, thirdQuest, finalQuest;
+    #endregion
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
