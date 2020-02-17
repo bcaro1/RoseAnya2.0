7288e5ce87cbc7a83577e059c1eba4231a397430
Alex <heitzalex@yahoo.com>
2020-02-17 09:38:39 -0500

    Fixing well bug + changing between cameras
    Well is able to be filled without water element.
    New script that will allow a camera change.

---
+++ Assets/Tech Team/Scripts/AlexScripts/CameraChange_Alex.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange_Alex : MonoBehaviour
+{
+    public Camera camera2;
+
+    void Awake()
+    {
+    }
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // if the player is out of radius
+        {
+            camera2.enabled = false;
+        }
+    }
+}
