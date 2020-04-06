32e26a0716d6ea5f6d83f34ac0d61f3c7631851f
Alex <heitzalex@yahoo.com>
2020-04-05 21:55:03 -0400

    FishingMinigame(WIP)

---
+++ Assets/Tech Team/AlexPrefabs/FishingMiniGame/PipeSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float waitTime;
+    public GameObject pipe;
+    public float height;
+    public Transform GamePanel;
+    private float timer = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {        
+        if (timer > waitTime)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            newPipe.transform.SetParent(GamePanel, false);
+            Destroy(newPipe, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
