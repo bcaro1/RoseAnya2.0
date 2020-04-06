da5e030011fe6db1b15df3138aee9c77d06d7ffb
Joseph Winemberg <42650208+WinembergJo@users.noreply.github.com>
2020-04-05 22:35:41 -0400

    Refactored Gauge System, Created More enemies, Created Pinpad puzzle

---
+++ Assets/Tech Team/Scripts/JosephScripts/Puzzles/PinpadTile_Joseph.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinpadTile_Joseph : MonoBehaviour
+{
+    public int number;
+    public bool isOn = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Strength"))
+        {
+            isOn = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Strength"))
+        {
+            isOn = false;
+        }
+    }
+}
--- Assets/Tech Team/Scripts/JosephScripts/Turnbased System/TBEnemySpawn.cs
+++ Assets/Tech Team/Scripts/JosephScripts/Turnbased System/TBEnemySpawn.cs
@@ -6,12 +6,14 @@ public class TBEnemySpawn : MonoBehaviour
 {
     public GameObject BattleSystem;
     public GameObject Enemy;
+    public Sprite Background;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             StaticDatabase_Joseph.Enemy = Enemy;
+            StaticDatabase_Joseph.BackGround = Background;
             Instantiate(BattleSystem);
             Destroy(gameObject);
         }
