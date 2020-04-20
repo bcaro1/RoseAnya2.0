90aacbf5b18dcfc49095632c1806c3de00b99183
Alex <heitzalex@yahoo.com>
2020-04-20 08:06:27 -0400

    More items + item pickup UI

---
+++ Assets/Tech Team/Scripts/AlexScripts/itemPickupUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemPickupUI : MonoBehaviour
+{
+    public GameObject textUI;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textUI.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textUI.SetActive(false);
+        }
+    }
+}
