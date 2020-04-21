69ab1b9617902a7664cd15ad069fcd4b410d38f9
Joseph Winemberg <42650208+WinembergJo@users.noreply.github.com>
2020-04-21 02:19:43 -0400

    Some menus now work with controller

---
+++ Assets/Tech Team/Scripts/JosephScripts/Menu/SelectOnInput_Joseph.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput_Joseph : MonoBehaviour
+{
+    public GameObject SelectedObject;
+    private EventSystem system;
+    private bool ButtonSelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        system = EventSystem.current;
+        system.SetSelectedGameObject(SelectedObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetAxisRaw("Vertical") != 0) && ButtonSelected == false)
+        {
+            system.SetSelectedGameObject(SelectedObject);
+            ButtonSelected = true;
+        }
+        if(system.currentSelectedGameObject == null)
+        {
+            system.SetSelectedGameObject(SelectedObject);
+        }
+    }
+
+    private void OnDisable()
+    {
+        ButtonSelected = false;
+    }
+
+}
